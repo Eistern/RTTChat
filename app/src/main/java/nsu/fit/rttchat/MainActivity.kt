71e8e8e637005e6ab3f02e43db194730eefbe358
d.zulin <d.zulin@g.nsu.ru>
2020-11-21 22:24:51 +0700

    Added test text input

--- app/src/main/java/nsu/fit/rttchat/MainActivity.kt
+++ app/src/main/java/nsu/fit/rttchat/MainActivity.kt
@@ -6,6 +6,7 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,7 +16,8 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.toolbar))
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            val text = findViewById<EditText>(R.id.nameInput).text
+            Snackbar.make(view, text, Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
     }
